92756db5ca81162dbc4dfe0f207091be01274b40
student <ivanovcharuk228@gmail.com>
2022-01-12 10:31:15 +0100

    Dodanie przycisków

---
+++ app/src/main/java/com/example/zadanieovcharuk/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.zadanieovcharuk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<ImageView>(R.id.obrazek).setImageResource(R.drawable.kot)
+        }
+    }
+}
